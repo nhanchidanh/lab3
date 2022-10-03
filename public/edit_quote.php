735d303dea7308c3ec7827a18562e58eb786abea
nhanchidanh <nhanchidanh@gmail.com>
2022-10-03 21:06:37 +0700

    cc

--- public/edit_quote.php
+++ public/edit_quote.php
@@ -8,7 +8,7 @@ echo '<h2>Sửa một Trích dẫn</h2>';
 include '../partials/check_admin.php';
 
 // echo '<p>Trang đang được xây dựng...</p>';
-include '../partials/db_connect.php';
+include '../partials/db_connect.php'; 
 
 if(isset($_GET['id']) && is_numeric($_GET['id']) && ($_GET['id'] > 0)) {
    $query = "SELECT quote, source, favorite FROM quotes WHERE id={$_GET['id']}";
@@ -23,8 +23,8 @@ if(isset($_GET['id']) && is_numeric($_GET['id']) && ($_GET['id'] > 0)) {
    if(!empty($row)) {
       ?>
       <form action="edit_quote.php" method="post">
-         <p><label for="">Trích dẫn <textarea name="quote" id="quote" cols="30" rows="5"><?php htmlspecialchars($row['quote']) ?></textarea></label></p>
-         <p><label for="">Nguồn <input type="text" name="source" id="source" value="<?php htmlspecialchars($row['source']) ?>"></label></p>
+         <p><label for="">Trích dẫn <textarea name="quote" id="quote" cols="30" rows="5"><?php echo htmlspecialchars($row['quote']) ?></textarea></label></p>
+         <p><label for="">Nguồn <input type="text" name="source" id="source" value="<?php echo htmlspecialchars($row['source']) ?>"></label></p>
          <p><label for="">Đây là trích dẫn được yêu thích? <input type="checkbox" name="favorite" id="favorite" value="yes" <?php if($row['favorite']) echo 'checked="checked"'; ?>></label></p>
          <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
          <p><input type="submit" name="submit" value="Cập nhật trích dẫn này!"></p>
